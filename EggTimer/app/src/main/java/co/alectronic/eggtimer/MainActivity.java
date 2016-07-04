8bda6eff9faaef4f2fd62dc8324ee965ce8cef4c
Alectronic <alec.game1@gmail.com>
2016-07-04 17:24:45 +0100

    Basic Egg time built Signed-off-by: Alectronic <alec.game1@gmail.com>

--- EggTimer/app/src/main/java/co/alectronic/eggtimer/MainActivity.java
+++ EggTimer/app/src/main/java/co/alectronic/eggtimer/MainActivity.java
@@ -1,5 +1,6 @@
 package co.alectronic.eggtimer;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public TextView txtTime;
     public SeekBar skbar;
     public int timerMS;
+    public CountDownTimer countDownTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,20 +27,54 @@ public class MainActivity extends AppCompatActivity {
         buttonState = 0;
         btnTime = (Button)findViewById(R.id.btnTime);
         txtTime = (TextView)findViewById(R.id.txtTime);
-        skbar = (SeekBar)findViewById(R.id.skbarTime)
+        skbar = (SeekBar)findViewById(R.id.skbarTime);
         btnTime.setText(getState(buttonState));
         timerMS = 1000;
+
+
+        skbar.setMax(600);
+        skbar.setProgress(30);
+        updateTime(30);
+
+        skbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {updateTime(i);}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
-
-
 
     public void buttonClick(View v){
         kickTimer();
         buttonState++;
         //Log.i("INFO",""+buttonState);
-        if(buttonState>2){buttonState=0;}
+        //if(buttonState>2){buttonState=0;}
+        if(buttonState>1){buttonState=0;}
         btnTime.setText(getState(buttonState));
     }
+
+
+    private void updateTime(int i){ txtTime.setText(getMin(i)+":"+getSecAsString(getRemainSecs(i)));  }
+
+    private int getMin(int sec){
+        return (int) sec / 60;
+    }
+
+    private int getRemainSecs(int sec){
+        return (int) sec % 60;
+    }
+
+    private String getSecAsString(int sec){
+        String tmpStr = ""+sec;
+        if(sec <= 9){ tmpStr = "0"+sec; }
+        return tmpStr;
+    }
+
+
 
     private void kickTimer(){
         switch (buttonState){
@@ -54,23 +91,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startTimer(){
+        skbar.setEnabled(false);
 
-        new CountDownTimer(10000,1000){
+        countDownTimer = new CountDownTimer(skbar.getProgress()*1000 + 100,1000){
             public void onTick(long millisecondUntilDone){//countdown is
                 Log.i("Second Left", String.valueOf(millisecondUntilDone / 1000));
-                timerMS = timerMS - 10;
-                txtTime.setText(""+timerMS);
+                updateTime((int)millisecondUntilDone / 1000);
             }
 
 
             public void onFinish(){//countdown is finished
                 Log.i("Done","ITS THE FINAL COUNTDOWN!");
+                //MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.noise);
+                //mplayer.start();
+                //
+                txtTime.setText("BOOM!");
             }
         }.start();
 
     }
+    private void resetTimer(){
+        skbar.setEnabled(true);
+        updateTime(skbar.getProgress());
+        countDownTimer.cancel();
+    }
+
     private void stopTimer(){}
-    private void resetTimer(){}
+
 
 
     private int a(){return 0;}
